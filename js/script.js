861f2c410a2f2649ea7eb53adee97aa39dfe66e6
MIDOZAVR <67678293+MIDOZAVR@users.noreply.github.com>
2020-07-07 20:32:00 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,64 @@
+const btn1 = $('.btn-1');
+const btn2 = $('.btn-2');
+const btn3 = $('.btn-3');
+const outText = $('.outtext');
+const var1Node = $('.var1');
+const var2Node = $('.var2');
+const var3Node = $('.var3');
+const var4Node = $('.var4');
+const var5Node = $('.var5');
+const var6Node = $('.var6');
+const speachNode = $('.speach');
+const inp = $('.input');
+
+const startObj = {
+	"text":[
+	"Жили-были {var1} да {var2}",
+	"Была у них {var3}",
+	"Снесла {var3} {var4}, не простое - золотое",
+	"- {var1} бил, бил - не разбил",
+	"- {var2} била, била - не разбила",
+	"{var5} бежала, {var6} задела, {var4} упало и разбилось.",
+	"{var1} плачет, {var2} плачет, а {var3} кудахчет:","{speach}"
+	]
+};
+
+btn1.click(function() {
+	outText.html(startObj.text);
+})
+
+btn2.click(function() {
+	const var1 = var1Node.val();
+	const var2 = var2Node.val();
+	const var3 = var3Node.val();
+	const var4 = var4Node.val();
+	const var5 = var5Node.val();
+	const var6 = var6Node.val();
+	const speach = speachNode.val();
+
+
+const newObj = {
+	"text":[
+	`Жили-были ${var1} да ${var2}`,
+	`Была у них ${var3}`,
+	`Снесла ${var3} ${var4}, не простое - золотое`,
+	`- ${var1} бил, бил - не разбил`,
+	`- ${var2} била, била - не разбила`,
+	`${var5} бежала, ${var6} задела, ${var4} упало и разбилось.`,
+	`${var1} плачет, ${var2} плачет, а ${var3} кудахчет:`,
+	`${speach}`
+	]
+};
+
+	outText.html(newObj.text);
+})
+
+btn3.click(function() {
+	const var1 = var1Node.val('');
+	const var2 = var2Node.val('');
+	const var3 = var3Node.val('');
+	const var4 = var4Node.val('');
+	const var5 = var5Node.val('');
+	const var6 = var6Node.val('');
+	const speach = speachNode.val('');
+})
